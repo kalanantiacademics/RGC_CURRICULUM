e187baaddc8ab9ce5c8a39ea3f86e54f5f98ba88
kalanantiacademics <kalanantiacademics@gmail.com>
2026-02-02 06:11:38 +0700

    Configure base path for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",  // Wajib untuk GitHub Pages
+  basePath: "/RGC_CURRICULUM",
+  assetPrefix: "/RGC_CURRICULUM/",
+  trailingSlash: true,
   images: {
     unoptimized: true, // Wajib jika nanti menggunakan komponen <Image /> Next.js
   },
